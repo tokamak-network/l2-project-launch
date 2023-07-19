238b8d8b83e2627cc3e61f6fe6985363fd576b62
HyukSang <zzooppii@gmail.com>
2023-07-19 11:09:59 +0900

    set the L2Public contract

---
+++ contracts/L2/vaults/L2PublicSaleVault.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "../../libraries/SafeERC20.sol";
+
+contract L2PublicSaleVault {
+    
+}
---
+++ contracts/L2/vaults/L2PublicSaleVaultProxy.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "../../proxy/Proxy.sol";
+import "./L2PublicSaleVaultStorage.sol";
+
+contract L2PublicSaleVaultProxy is Proxy, L2PublicSaleVaultStorage
+{
+
+}
