f9a036b3eb258374a58a3f0384b5d89f89a98130
HyukSang <zzooppii@gmail.com>
2023-07-19 16:28:09 +0900

    update storage

--- contracts/L2/vaults/L2PublicSaleVault.sol
+++ contracts/L2/vaults/L2PublicSaleVault.sol
@@ -3,6 +3,42 @@ pragma solidity ^0.8.4;
 
 import "../../libraries/SafeERC20.sol";
 
-contract L2PublicSaleVault {
-    
+import { ProxyStorage } from "../../proxy/ProxyStorage.sol";
+import { AccessibleCommon } from "../../common/AccessibleCommon.sol";
+import { L2CustomVaultBaseStorage } from "./L2CustomVaultBaseStorage.sol";
+
+contract L2PublicSaleVault is 
+    ProxyStorage,
+    AccessibleCommon, 
+    L2CustomVaultBaseStorage 
+{
+    using SafeERC20 for IERC20;
+
+     /* ========== onlyOwner ========== */
+    function setL2ProjectManager(address _l2ProjectManager)
+        external nonZeroAddress(_l2ProjectManager) onlyOwner
+    {
+        require(l2ProjectManager != _l2ProjectManager, "same");
+        l2ProjectManager = _l2ProjectManager;
+    }
+
+    /* ========== only L2ProjectManager ========== */
+
+    function setVaultAdmin(
+        address l2Token,
+        address _newAdmin
+    )
+        external nonZeroAddress(l2Token) nonZeroAddress(_newAdmin) onlyL2ProjectManager
+    {
+        require(vaultAdminOfToken[l2Token] != _newAdmin, "same");
+        vaultAdminOfToken[l2Token] = _newAdmin;
+        emit SetVaultAdmin(l2Token, _newAdmin);
+    }
+
+    /* ========== VIEW ========== */
+
+    function isVaultAdmin(address l2Token, address account) public view returns (bool) {
+        return (account != address(0) && vaultAdminOfToken[l2Token] == account);
+    }
+
 }
--- contracts/L2/vaults/L2PublicSaleVaultStorage.sol
+++ contracts/L2/vaults/L2PublicSaleVaultStorage.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import { LibPublicSaleVault } from "../../libraries/LibPublicSaleVault.sol";
 
 contract L2PublicSaleVaultStorage {
     uint24 public constant poolFee = 3000;
@@ -8,6 +9,7 @@ contract L2PublicSaleVaultStorage {
     address public quoter;
     address public vestingFund;
     address public liquidityVault;
+    address public uniswapRouter;
     
     address public lockTOS;
     address public tos;
@@ -23,7 +25,50 @@ contract L2PublicSaleVaultStorage {
     uint256 public stanTier3;     //최소 기준 Tier3
     uint256 public stanTier4;     //최소 기준 Tier4 기준이 제일 큼
 
+    //관리자가 설정하는 changeTick
+    int24 public changeTick;    //TON -> TOS로 변경할때 허용되는 Tick 범위
+
     //관리자가 설정하는 contract를 deploy 후 snapshot을 지정할 수 있는 최소 시간 간격
     uint256 public delayTime;
+
+    //L2PublicSale을 manage하는 manager
+    address public l2ProjectManager;
     
+    bool internal free = true;
+    
+    //l2token이 기준이 된다. (l2token - tokenOwner)
+    mapping(address => address) public vaultAdminOfToken;
+
+    // l2token - timeInfo
+    mapping(address => LibPublicSaleVault.TokenTimeManage) public timeInfo;
+
+    // l2token - manageInfo
+    mapping(address => LibPublicSaleVault.TokenSaleManage) public manageInfo;
+
+    modifier onlyL2ProjectManager() {
+        require(l2ProjectManager != address(0) && msg.sender == l2ProjectManager, "caller is not l2ProjectManager");
+        _;
+    }
+
+    modifier onlyVaultAdminOfToken(address l2token) {
+        require(vaultAdminOfToken[l2token] != address(0) && msg.sender == vaultAdminOfToken[l2token], "caller is not a vaultAdmin Of l2Token");
+        _;
+    }
+
+    modifier nonZero(uint256 value) {
+        require(value != 0, "Z1");
+        _;
+    }
+
+    modifier nonZeroAddress(address account) {
+        require(account != address(0), "Z2");
+        _;
+    }
+
+    modifier ifFree {
+        require(free, "lock");
+        free = false;
+        _;
+        free = true;
+    }
 }
