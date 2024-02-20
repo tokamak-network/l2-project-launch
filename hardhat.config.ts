1a47c0d15330a5b1faa11699c11adb8cf7a0680c
Zena-park <zena@tokamak.network>
2024-02-20 13:13:36 +0900

    artifacts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -160,10 +160,10 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
-      // forking: {
-      //   url: `${process.env.ETH_NODE_URI_GOERLI}`,
-      //   blockNumber: 9448400
-      // },
+      forking: {
+        url: `${process.env.ETH_NODE_URI_GOERLI}`,
+        blockNumber: 9448400
+      },
       allowUnlimitedContractSize: false,
       // deploy: ['deploy_l1', 'deploy_l2'],
       // deploy: ['deploy_l1'],
